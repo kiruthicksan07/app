db5fa7ad39eca87f16c33b38765e3bbf2463e89f
kiruthicksan06 <140225861+kiruthicksan06@users.noreply.github.com>
2023-07-24 22:49:01 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template , jsonify
+
+app = Flask(__name__)
+JOBS =[
+  {
+    'id' : 1,
+    'title':'Data Analyst',
+    'location' : 'Bengaluru',
+    'salary': '1,00,000'
+  },
+  {
+    'id' : 2,
+    'title':'Data Scientist',
+    
+    'salary': '1,50,000'
+  },
+   {
+    'id' : 3,
+    'title':'Frontend Developer',
+    'location' : 'Bengaluru',
+    'salary': '1,60,000'
+  },
+   {
+    'id' : 4,
+    'title':'Backend Developer',
+    'location' : 'Bengaluru',
+    'salary': '1,80,000'
+  }
+]
+
+
+
+@app.route("/api/")
+def home():
+  return render_template('home.html' , jobs = JOBS)
+@app.route("/")
+def joblist():
+  return jsonify(JOBS)
+
+                          
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
